88d25f7389b0ec32efa0d6cfd8a900f68930f6db
Vishal Musale <vishal.m.musale@gmail.com>
2017-02-16 14:58:07 -0800

    added - generic function sending messages into index.js

--- index.js
+++ index.js
@@ -38,6 +38,26 @@ app.post('/webhook', function (req, res) {
     res.sendStatus(200);
 })
 
+
+// generic function sending messages
+function sendMessage(recipientId, message) {  
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
