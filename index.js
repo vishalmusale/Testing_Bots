7fad004e63cce1e3190ebee1d0ae20a285b6b678
Vishal Musale <vishal.m.musale@gmail.com>
2017-02-18 14:43:27 -0800

    Code is Working

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello , I am a chat bot')
+    res.send('Testing Shantabai Bot')
 })
 
 // for Facebook verification
@@ -43,7 +43,7 @@ app.post('/webhook/', function (req, res) {
                 sendGenericMessage(sender)
                 continue
             }
-            sendTextMessage(sender, "parrot: " + text.substring(0, 200))
+            sendTextMessage(sender, "ShaantaBai: " + text.substring(0, 200))
         }
         if (event.postback) {
             text = JSON.stringify(event.postback)
