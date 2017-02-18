e569bd774150656f8d3cefdb6248a89048a81c72
Vishal Musale <vishal.m.musale@gmail.com>
2017-02-17 18:58:56 -0800

    changed token

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.post('/webhook/', function(req, res) {
 function sendMessage(recipientId, message) {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
         method: 'POST',
         json: {
             recipient: {id: recipientId},
